dab9b97b65b7ba9c22df11444b7ff592c5a36ae2
Borja Armet <borjaarmet@gmail.com>
2021-04-09 13:31:15 +0200

    lab completed

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        //for (let i = 0; i < this.length; i++) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b)
+        this.length++;
+        //}
 
-  get(pos) {}
+    }
 
-  max() {}
+    get(pos) {
+        this.items.indexOf(pos);
+        if (pos > this.length) {
+            throw new Error('Out of Bounds')
+        } else {
+            return pos;
+        }
+    }
 
-  min() {}
+    max() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList')
+        } else {
+            return Math.max(...this.items)
+        }
 
-  sum() {}
+    }
 
-  avg() {}
+    min() {
+
+        if (this.length === 0) {
+            throw new Error('EmptySortedList')
+        } else {
+            return Math.min(...this.items)
+        }
+    }
+
+    sum() {
+
+        if (this.length === 0) {
+            return 0
+        } else {
+            let sum = this.items.reduce((a, b) => a + b, 0);
+            return sum
+        }
+    }
+
+    avg() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList')
+        } else {
+            let sum = this.items.reduce((a, b) => a + b, 0);
+            return sum / this.length;
+        }
+
+    }
 }
 
 module.exports = SortedList;
